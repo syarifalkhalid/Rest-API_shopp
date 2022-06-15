ba6eed5734622a5d8c73300220f66d4617249e59
Jansfy_ <ibjansyarifhidayatullah240101@gmail.com>
2022-06-15 16:06:46 +0700

    membuat seeder costumer,products

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Products;
 use Illuminate\Database\Seeder;
+use Faker\Factory as DataPalsu;
+use Illuminate\Support\Facades\Hash;
 
 class ProductSeeder extends Seeder
 {
@@ -13,6 +16,44 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $dataPalsu = DataPalsu::create('id_ID');
+        
+        $categories = ['Pakaian', 'Gadget', 'Digital'];
+        
+        $titles =[
+            'Pakaian'   =>[
+                'material' => ['kaos', 'celana', 'kemeja', 'jas'],
+                'jenis' => ['Laki-Laki', 'Perempuan', 'Anak', 'Dewasa'],
+                'warna' => ['Merah', 'Kuning', 'Hijau', 'Biru'],
+            ],
+            'Gadget'   =>[
+                'material' => ['HP', 'Notebook', 'Laptop', 'Tablet'],
+                'jenis' => ['Asus', 'Acer', 'Thosiba', 'Lenovo'],
+                'warna' => ['Black', 'Blue', 'White', 'Silver'],
+            ],
+            'Digital'   =>[
+                'material' => ['Pulsa', 'Kuota', 'Voucer', 'Kartu'],
+                'jenis' => ['XL', 'Telkomsel', 'Axis', 'Indosat','3'],
+                'warna' => ['100', '80', '50', '25', '10'],
+            ]
+        
+        ];
+        for ($i=0; $i <40 ; $i++) { 
+            $category		= $dataPalsu->randomElement($categories);
+            $titleStr		= $dataPalsu->randomElement( $titles[$category]['material']);
+            $titleStr		.= ' ' . $dataPalsu->randomElement( $titles[$category]['jenis']);
+            $titleStr		.= ' ' . $dataPalsu->randomElement( $titles[$category]['warna']);
+
+            $data[] = [
+                'category'      => $category,
+                'title'         => $titleStr,
+                'price'         => $dataPalsu->numberBetween(1,100)*1000,
+                'description'  => $dataPalsu->text(),
+                'stock'         => $dataPalsu->numberBetween(1,100),
+                'free_shipping' => $dataPalsu->numberBetween(0,1),
+                'rate'          => $dataPalsu->randomFloat(2,1,5),
+            ];
+        }
+        (new Products())->insert($data);
     }
 }
